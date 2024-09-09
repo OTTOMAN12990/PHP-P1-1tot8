187d30c561d6661d1e240b0acb7ad6de4cc953d6
OTTOMAN12990 <9014996@student.zadkine.nl>
2024-09-09 11:56:57 +0200

    Opdracht 1.1
    opdracht 1.1 is klaar. meschien zijn de uitslagen niet exact zoals in de opdracht maar de codes werken wel en ik wil het uniek houden zoals ik mijn codes wil hebben. de code is precies het zelfde maar de uitslag is gepersonaliseerd

---
+++ Opdracht-1.1/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// 1. String
+$name = "januari";
+echo $name; // Output: januari
+
+// 2. Integer
+$age = 22;
+echo $age; // Output: 22
+
+// 3. Float
+$price = 1.000.000,00;
+echo $price; // Output: 1.000.000,00
+
+// 4. Boolean
+$isStudent = true;
+echo $isStudent; // Output: 1 (true wordt weergegeven als 1)
