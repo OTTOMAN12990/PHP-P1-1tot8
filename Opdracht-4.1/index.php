5511f7920c2c1b7baf149729db1dc79535fb1ca2
OTTOMAN12990 <9014996@student.zadkine.nl>
2024-09-10 13:28:56 +0200

    opdracht 4.1

--- Opdracht-4.1/index.php
+++ Opdracht-4.1/index.php
@@ -1,9 +1,16 @@
 <?php
 
 $products = [
-    ["bmw", m4, "2"],
-    ["audi", rs7, "4"],
-    ["volkswagen", golf, "20"]
-    ["maserati", ghibli, "7"]
+    ["bmw", "m4", "2", "75.120"],
+    ["audi", "rs7", "4", "88.459"],
+    ["volkswagen", "golf", "20", "24.999"],
+    ["maserati", "ghibli", "7", "64.999"],
+    ["Tablet", "GalaxyTab 9", "50", "299.99"]
+];
 
-]
+$products[1][0] = "Ferrari";
+$products[1][1] = "F40";
+$products[1][2] = "3";
+$products[3][2] = "120";
+
+var_dump($products);
