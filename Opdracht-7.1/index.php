cdb4ae54a3cd1da9ed1f6bf5a15e6b0dd059ac90
OTTOMAN12990 <9014996@student.zadkine.nl>
2024-09-17 14:05:55 +0200

    7.1

---
+++ Opdracht-7.1/index.php
@@ -0,0 +1,5 @@
+<form method="POST" action="verwerk.php">
+    Geboortedatum: <input type="date" name="geboortedatum" required><br>
+    Telefoonnummer : <input type="number" name="telefoonnummer" required><br>
+    <input type="submit" value="Verstuur">
+</form>
