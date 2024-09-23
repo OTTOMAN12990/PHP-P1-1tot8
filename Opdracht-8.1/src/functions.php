c50f737a9d497adf2fd5684c63c06e6f7fbff903
OTTOMAN12990 <9014996@student.zadkine.nl>
2024-09-23 13:10:35 +0200

    j

---
+++ Opdracht-8.1/src/functions.php
@@ -0,0 +1,16 @@
+<?php
+ 
+function sanitize_input($data) {
+
+    $data = trim($data);
+
+    $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+ 
+    return $data;
+ 
+    function format_input($data) {
+
+        return strtoupper($data);
+    }
+   
+}
